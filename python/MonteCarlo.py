00f887ae72a828cb85db682330eef2e6702468e9
Nero Zuo <502350123@qq.com>
2016-12-24 17:28:31 +0800

    MonteCarlo update

--- python/MonteCarlo.py
+++ python/MonteCarlo.py
@@ -1,4 +1,6 @@
 import datetime
+from random import choice
+from __futrue__ import division
 
 class MonteCarlo(object):
     """docstring for MonteCarlo"""
@@ -29,6 +31,9 @@ class MonteCarlo(object):
     def run_simulation(self):
         # Plays out a "random" game from the current position,
         # then updates the statistics tables with the result.
+        plays, wins = self.plays, self.wins
+
+
         visited_states = set()
         states_copy = self.states[:]
         state = states_copy[-1]
@@ -39,14 +44,29 @@ class MonteCarlo(object):
         for t in xrange(self.max_moves):
             legal = self.board.legal_plays(states_copy)
             
-            play = choice(legal)
-            state = self.board.next_state(state, play)
+            move_states = [(p, self.board.next_state(state, p)) for p in legal]
+            if all(plays.get((player, S)) for p, S in move_states):
+                log_total = log(sum(plays[(player, S)] for p, S in move_states))
+                value, move, state = max(
+                        ((wins[player, S]/plays[player, S]) +
+                            self.C * sqrt(log_total / plays[player, S]), p, S)
+                        for p, S in move_states
+                    )
+            else:
+                # Otherwise, just make an arbitrary decision.
+                move, state = choice(moves_states)
+
+            # play = choice(legal)
+            # state = self.board.next_state(state, play)
             states_copy.append(state)
 
             if expend and (player, state) not in self.plays:
                 expend = False
                 self.plays[(player, state)] = 0
                 self.wins[(player, state)] = 0
+
+                if t > self.max_depth:
+                    self.max_depth = t
 
             visited_states.add((player, state))
 
@@ -56,14 +76,57 @@ class MonteCarlo(object):
             if winner:
                 break
 
-            for player, state in visited_states:
-                if (player, state) not in self.plays:
-                    continue
-                self.plays[(player, state)] += 1
-                if player == winner:
-                    self.wins[(player, states)] += 1
+        for player, state in visited_states:
+            if (player, state) not in self.plays:
+                continue
+            self.plays[(player, state)] += 1
+            if player == winner:
+                self.wins[(player, states)] += 1
 
     def get_play(self):
+        
+        self.max_depth = 0
+        state = self.states[-1]
+        player = self.board.current_player(state)
+        legal = self.board.legal_plays(self.states[:])
+
+        # Bail out early if there is no real choice to be made.
+        if not legal:
+            return
+        if len(legal) == 1:
+            return legal[0]
+
+        games = 0
         begin = datetime.datetime.utcnow()
         while datetime.datetime.utcnow() - begin < self.calculation_time:
             self.run_simulation()
+            games += 1
+
+        move_states = [(p, self.board.next_state(state, p)) for p in legal]
+
+        # Display the number of calls of `run_simulation` and the
+        # time elapsed.
+        print games, datetime.datetime.utcnow() - begin
+
+        # Pick the move with the highest percentage of wins.
+        percent_wins, move = max(
+            (self.wins.get((player, S), 0) /
+             self.plays.get((player, S), 1),
+             p)
+            for p, S in moves_states
+        )
+
+         # Display the stats for each possible play.
+        for x in sorted(
+            ((100 * self.wins.get((player, S), 0) /
+              self.plays.get((player, S), 1),
+              self.wins.get((player, S), 0),
+              self.plays.get((player, S), 0), p)
+             for p, S in moves_states),
+            reverse=True
+        ):
+            print "{3}: {0:.2f}% ({1} / {2})".format(*x)
+
+        print "Maximum depth searched:", self.max_depth
+
+        return move
